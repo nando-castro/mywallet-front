9d03314e4d683b2e385dc464ed59a816885cd8ad
nando-castro <fernando.castro10.master@gmail.com>
2022-06-28 22:30:30 -0300

    screen login

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+import Login from "./home/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login /* setToken={setToken} */ />} />
+        {/* <Route path="/sign-up" element={<Register />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/home/Login.js
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../../assets/img/logo.png";
+//import axios from 'axios';
+
+function Login() {
+  return (
+    <Home>
+      <img src={logo} alt="MyWallet" />
+      <Form>
+        <input
+          type="text"
+          placeholder="E-mail"
+          required /* value={email} onChange={(e) => setEmail(e.target.value)} */
+        />
+        <input
+          type="text"
+          placeholder="Senha"
+          required /* value={password} onChange={(e) => setPassword(e.target.value)} */
+        />
+      </Form>
+      <div className="button" /* onClick={handleLogin} */>Entrar</div>
+      <Link to="/sign-up">
+        <p>Primeira vez? Cadastre-se!</p>
+      </Link>
+    </Home>
+  );
+}
+
+const Home = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: #8c11be;
+
+  img {
+    width: 147px;
+    height: 50px;
+    margin-bottom: 32px;
+  }
+
+  input {
+    width: 80%;
+    height: 45px;
+    background: #a328d6;
+    border: 1px solid #d5d5d5;
+    border-radius: 5px;
+    margin-bottom: 6px;
+
+    background: #ffffff;
+    border-radius: 5px;
+
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+
+    color: #000000;
+  }
+
+  .button {
+    width: 80%;
+    heigth: 45px;
+    padding: 10px;
+
+    background: #a328d6;
+    border-radius: 5px;
+
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 23px;
+
+    color: #ffffff;
+  }
+
+  p {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 18px;
+
+    color: #ffffff;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export default Login;
